7f0c0518aa50d0be8441a16830fae8f678a1c0a9
Anthony Braun <antmbraun@gmail.com>
2023-12-13 23:37:36 -0500

    day13 - fix

--- day13/reflection.py
+++ day13/reflection.py
@@ -5,16 +5,17 @@ result = 0
 
 def find_lines_before_reflection(matrix):
   back = 1
-  r = 2 
+  r = 1
   while r < len(matrix):
     if matrix[r] == matrix[r - back]:
       line_of_reflection = r
     while matrix[r] == matrix[r - back]:
       back += 2
       r += 1
-      if r == len(matrix) or r - back < 0:
+      if r == len(matrix) or r - back <0:
         return line_of_reflection
-    r+=1
+    back = 1
+    r += 1
   return 0
 
 for pattern in patterns:
@@ -34,4 +35,3 @@ for pattern in patterns:
   result += find_lines_before_reflection(rows) * 100 + find_lines_before_reflection(cols)
 
 print(result)
-
